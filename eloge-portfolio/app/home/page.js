c152bf22d3a3bdb4352bccde7d6268ae8b2e8aaf
0122345 <ntwarifiacre043@gmail.com>
2024-08-31 16:28:19 +0200

    setting environment

--- eloge-portfolio/app/home/page.js
+++ eloge-portfolio/app/home/page.js
@@ -1,14 +1,36 @@
- 
+function HomePage() {
+  return (
+    <main className={khand.className}>
+      <div className="bg-gradient-to-r from-home-gray from-50% to-home-yellow to-50% w-dvw h-dvh">
+        <div id="left" className="flex flex-col text-white justify-start w-[50%]">
+          <span className="">
+            <p className="">
+               Hello
+            </p>
+            <p className="">
+              I'M ELOGE CHRIS
+            </p>
+            <p className="">
+            Welcome to my creative world, 
+            a passionate designer fueled 
+            by a relentless drive for innovation 
+            and a love for crafting captivating 
+            experiences.
+            </p>
+          </span>
 
-function HomePage () {
-   return(
-    <main className="text-4xl w-dvw h-dvh bg-gray-500 text-green-400">
-      <h1>Welcome to Eloge's Portfolio</h1>
-      <p>This is the home page of my personal portfolio.</p>
-      <p>Please explore the other pages to learn more about me.</p>
-      <h1>Home Page</h1>
+          {/* button */}
+          <span></span>
+        </div>
+      </div>
     </main>
-   );
+  );
 }
 
 export default HomePage;
+import { Khand } from 'next/font/google';
+
+const khand = Khand({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
--- eloge-portfolio/tailwind.config.js
+++ eloge-portfolio/tailwind.config.js
@@ -7,11 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+       colors: {
+        'home-gray': '#3F3F3F',
+        'home-yellow': '#E9B137'
+       }
     },
   },
   plugins: [],
