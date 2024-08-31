750a3fe9c1c9e69a7d689d539a39ec026821fd8a
0122345 <ntwarifiacre043@gmail.com>
2024-08-31 20:00:52 +0200

    half homepage

--- eloge-portfolio/app/components/Footer.js
+++ eloge-portfolio/app/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full min-h-screen text-white bg-black px-8 md:px16 py-16'>
+    <div className='w-full min-h-screen text-white bg-black px-8 md:px16 py-16 font-khand'>
         <h1>Contact me</h1>
     </div>
   )
--- eloge-portfolio/app/home/page.js
+++ eloge-portfolio/app/home/page.js
@@ -1,17 +1,18 @@
 import { BsThreeDots } from "react-icons/bs";
+
 function HomePage() {
   return (
     <main className={khand.className}>
-      <div className="bg-gradient-to-r from-home-gray from-50% to-home-yellow to-50% w-dvw h-dvh">
+      <div className="bg-gradient-to-r from-home-gray from-50% to-home-yellow to-50% w-dvw h-dvh overflow-hidden">
         <div id="left" className="flex flex-col text-white justify-start w-[50%]">
-          <span className="">
-            <p className="">
+          <span className="w-[60%] h-[65%] mx-20 space-y-2 my-32">
+            <p className="text-4xl font-light">
                Hello
             </p>
-            <p className="">
+            <p className="text-home-yellow text-6xl font-bold">
               I'M ELOGE CHRIS
             </p>
-            <p className="">
+            <p className="text-xl font-light">
             Welcome to my creative world, 
             a passionate designer fueled 
             by a relentless drive for innovation 
@@ -21,25 +22,25 @@ function HomePage() {
           </span>
 
           {/* button */}
-          <span className=''>
+          <span className='mx-20 top-[-62px] relative border border-home-yellow border-solid w-36 h-11 shadow-inner  shadow-[#383636] flex items-center justify-center text-2xl font-medium text-home-yellow cursor-pointer hover:bg-home-yellow hover:text-home-gray'>
              Hire Me
           </span>
         </div>
-        <div id='center' className=''>
+        <div id='center' className='relative flex justify-center left-[6rem]'>
           <Image
            src="/assets/images/eloge-removebg-preview 2eloge.png"
            alt='home photo'
-           width={527}
-           height={663}
+           width={472}
+           height={363}
          //   layout="responsive"
            objectFit="cover"
            priority
-           className="flex justify-center top-0 absolute"
+           className="top-0 absolute"
           />
         </div>
          <div id='right' className='flex float-right'>
             <span>
-            <BsThreeDots  className="text-white text-9xl right-20 "/>
+            <BsThreeDots  className="text-white text-5xl mr-16"/>
 
             </span>
 
