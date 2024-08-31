16859fddee0fc24d5902346f0fb00aad42852a05
0122345 <ntwarifiacre043@gmail.com>
2024-08-31 20:27:41 +0200

    homepage done

--- eloge-portfolio/app/home/page.js
+++ eloge-portfolio/app/home/page.js
@@ -1,8 +1,8 @@
 import { BsThreeDots } from "react-icons/bs";
 
-function HomePage() {
+ export default function HomePage() {
   return (
-    <main className='font-khand w-full'>
+    <main className='font-khand w-full overflow-hidden'>
       <div className="bg-gradient-to-r from-home-gray from-50% to-home-yellow to-50% w-dvw h-dvh">
         <div id="left" className="flex flex-col text-white justify-start w-[50%]">
           <span className="w-[60%] h-[65%] mx-20 space-y-2 my-32">
@@ -26,25 +26,21 @@ function HomePage() {
              Hire Me
           </span>
         </div>
-        <div id='center' className='relative flex justify-center left-[6rem]'>
+        <div id='center' className='flex right-0'>
+
+           <span className="top-4 flex justify-end absolute float-right right-2"> 
+            <BsThreeDots  className="text-white text-6xl mr-16"/>
+            </span>
           <Image
            src="/assets/images/eloge-removebg-preview 2eloge.png"
            alt='home photo'
-           width={472}
-           height={363}
-         //   layout="responsive"
+           width={595}
+           height={668}
            objectFit="cover"
-           priority
-           className="top-0 absolute"
+           priority 
+           className="absolute top-[-85px] flex justify-end float-right left-[34pc]"
           />
         </div>
-         <div id='right' className='flex float-right'>
-            <span>
-            <BsThreeDots  className="text-white text-5xl mr-16"/>
-
-            </span>
-
-            </div>
       </div>
     </main>
   );
