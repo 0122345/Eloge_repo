006b1aa016b444d1d2661dd563619e640c7d0056
0122345 <ntwarifiacre043@gmail.com>
2024-09-01 22:59:43 +0200

    finish up homepage & about me with responsiveness

--- eloge-portfolio/app/components/About.js
+++ eloge-portfolio/app/components/About.js
@@ -1,26 +1,55 @@
 import Image from "next/image";
+import 'lazysizes';
 
-export default  function About() {
-   return(
-    <main className="h-dvh w-full bg-[#393939]">
-        {/* left */}
-         <div className="w-[40dvw] h-dvh" >
-            <div id="image-potrait" className="">
-                <span id="image-holder" className="">
-                    <Image
-                      src="/assets/images/eloge-removebg-preview 2eloge.png"
-                      alt='home photo'
-                      width={185}
-                      height={268}
-                      objectFit="cover"
-                      priority 
-                    />
-                </span>
-            </div>
-         </div>
+export default function About() {
+  return (
+    <main className="flex flex-col lg:flex-row min-h-screen w-full bg-[#393939] font-khand p-4 lg:p-8 overflow-x-hidden">
+      <div id="container" className="relative flex justify-center lg:justify-start lg:ml-[8vw] lg:mt-[10vh] mb-8 lg:mb-0">
+        <div className="w-[200px] lg:w-[250px] border-[10px] border-solid border-home-yellow h-[270px] lg:h-[320px]">
+        </div>
+        <div id="image-potrait" className="absolute top-[-10px] lg:top-[-20px] left-[10px] lg:left-[20px] w-[200px] lg:w-[250px] z-10 h-[270px] lg:h-[320px] bg-home-yellow border-[8px] lg:border-[12px] border-solid border-white">
+          <span id="image-holder" className="flex items-center justify-center w-full h-full">
+            <Image
+              src="/assets/images/eloge-removebg-preview 2eloge.png"
+              alt='home photo'
+              width={300}
+              height={450}
+              objectFit="contain"
+              priority
+              className="lazy-loading"
+            />
+          </span>
+        </div>
+      </div>
 
-         {/* right */}
-         <div className="w-[60dvw] h-dvh"></div>
+      <div className="w-full lg:w-[50%] flex flex-col lg:ml-[10vw] mt-8 lg:mt-[10vh]">
+        <div className="flex flex-col my-5">
+          <h1 className="text-2xl lg:text-3xl text-white font-semibold">About Me</h1>
+          <hr className="w-28 lg:w-36 border-t-4 border-home-yellow my-3 lg:my-5" />
+          <p className="text-lg lg:text-xl text-white w-full lg:max-w-[500px] pb-6 lg:pb-11">
+            A passionate designer with a love for crafting
+            meaningful and visually stunning experiences.
+            With a background, e.g. graphic design, digital
+            Design etc , I thrive on bringing creativity to
+            life through thoughtful design solutions.
+          </p>
+          <p className="text-lg lg:text-xl text-white w-full lg:max-w-[450px]">
+            Whether it's creating captivating visuals or
+            designing different user experiences, I'm dedicated
+            to exceeding expectations and making a lasting impact.
+            Let's collaborate and bring your vision to life.
+          </p>
+
+          <div className="flex flex-col sm:flex-row my-12 lg:my-24 pt-8 lg:pt-16 space-y-4 sm:space-y-0 sm:space-x-4 lg:space-x-11">
+            <span className='border border-home-yellow border-solid rounded-sm w-full sm:w-36 h-11 shadow-inner shadow-[#383636] flex items-center justify-center text-xl lg:text-2xl font-medium hover:bg-home-gray hover:text-home-yellow cursor-pointer bg-home-yellow text-white'>
+              Hire Me
+            </span>
+            <span className='border border-home-yellow border-solid rounded-sm w-full sm:w-44 h-11 shadow-inner shadow-[#383636] flex items-center justify-center text-xl lg:text-2xl font-medium text-home-yellow cursor-pointer hover:bg-home-yellow hover:text-home-gray'>
+              Discover All
+            </span>
+          </div>
+        </div>
+      </div>
     </main>
-   );
+  );
 }
--- eloge-portfolio/app/home/page.js
+++ eloge-portfolio/app/home/page.js
@@ -1,49 +1,50 @@
 import { BsThreeDots } from "react-icons/bs";
+import Image from "next/image";
 
- export default function HomePage() {
+export default function HomePage() {
   return (
     <main className='font-khand w-full overflow-hidden'>
-      <div className="bg-gradient-to-r from-home-gray from-50% to-home-yellow to-50% w-dvw h-dvh">
-        <div id="left" className="flex flex-col text-white justify-start w-[50%]">
-          <span className="w-[60%] h-[65%] mx-20 space-y-2 my-32">
-            <p className="text-4xl font-light">
-               Hello
-            </p>
-            <p className="text-home-yellow text-6xl font-bold">
-              I'M ELOGE CHRIS
-            </p>
-            <p className="text-xl font-light">
-            Welcome to my creative world, 
-            a passionate designer fueled 
-            by a relentless drive for innovation 
-            and a love for crafting captivating 
-            experiences.
-            </p>
-          </span>
-
-          {/* button */}
-          <span className='mx-20 top-[-62px] relative border border-home-yellow border-solid w-36 h-11 shadow-inner  shadow-[#383636] flex items-center justify-center text-2xl font-medium text-home-yellow cursor-pointer hover:bg-home-yellow hover:text-home-gray'>
-             Hire Me
-          </span>
-        </div>
-        <div id='center' className='flex right-0'>
-
-           <span className="top-4 flex justify-end absolute float-right right-2"> 
-            <BsThreeDots  className="text-white text-6xl mr-16"/>
+      <div className="bg-gradient-to-t from-home-yellow from-50% to-home-gray to-50% w-full min-h-screen lg:h-screen lg:bg-gradient-to-r lg:from-home-gray lg:from-50% lg:to-home-yellow lg:to-50%">
+        <div className="flex flex-col lg:flex-row">
+          <div id="left" className="flex flex-col text-white justify-start w-full lg:w-1/2 p-6 lg:p-20">
+            <span className="space-y-2 my-8 lg:my-32">
+              <p className="text-3xl lg:text-4xl font-light">
+                Hello
+              </p>
+              <p className="text-home-yellow text-5xl lg:text-6xl font-bold">
+                I'M ELOGE CHRIS
+              </p>
+              <p className="text-lg lg:text-xl font-light">
+                Welcome to my creative world, 
+                a passionate designer fueled 
+                by a relentless drive for innovation 
+                and a love for crafting captivating 
+                experiences.
+              </p>
             </span>
-          <Image
-           src="/assets/images/eloge-removebg-preview 2eloge.png"
-           alt='home photo'
-           width={595}
-           height={668}
-           objectFit="cover"
-           priority 
-           className="absolute top-[-85px] flex justify-end float-right left-[34pc]"
-          />
+
+            {/* button */}
+            <span className='mt-6 lg:mt-0 border border-home-yellow border-solid rounded-sm w-36 h-11 shadow-inner shadow-[#383636] flex items-center justify-center text-2xl font-medium text-home-yellow cursor-pointer hover:bg-home-yellow hover:text-home-gray lg:hover:bg-home-gray lg:hover:text-home-yellow transition-colors duration-300 ease-in-out'>
+              Hire Me
+            </span>          
+          </div>
+          <div id='center' className='relative w-full lg:w-1/2 mt-8 lg:mt-0'>
+            <span className="absolute top-4 right-6 lg:right-16"> 
+              <BsThreeDots className="text-white text-4xl lg:text-6xl"/>
+            </span>
+            <Image
+              src="/assets/images/eloge-removebg-preview 2eloge.png"
+              alt='home photo'
+              width={400}
+              height={450}
+              layout="responsive"
+              objectFit="contain"
+              priority 
+              className="mx-auto lg:absolute lg:top-[-85px] lg:right-[145px]"
+            />
+          </div>
         </div>
       </div>
     </main>
   );
 }
-
-import Image from "next/image";
